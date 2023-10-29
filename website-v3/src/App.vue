3974569ba3e44f2f5f2fd2319117c32a0b9af5b9
vikram <66767382+vihk@users.noreply.github.com>
2023-10-29 01:03:28 -0700

    SUCCYSESS

--- website-v3/src/App.vue
+++ website-v3/src/App.vue
@@ -4,8 +4,14 @@
       <input v-model="textInput" class="input-field">
       <button class="submit-button">Submit</button>
     </form>
-
-    <div class="response">Response: {{ response }}</div>
+    <div class="response">
+      <div v-for="(segment, index) in formattedResponseSegments" :key="index" class="response-box">
+        <p class="response-title">{{ segment[0] }}</p>
+        <ul class="response-list">
+          <li v-for="(item, itemIndex) in segment.slice(1)" :key="itemIndex">{{ item }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,30 +20,47 @@ export default {
   data() {
     return {
       textInput: '',
-      response: null
-    }
+      response: null,
+    };
   },
-
+  computed: {
+    formattedResponseSegments() {
+      if (this.response && this.response.result) {
+        const result = this.response.result;
+        let segments = [];
+        for (const key in result) {
+          const subObject = result[key];
+          let segment = [key];
+          for (const subKey in subObject) {
+            segment.push(`${subKey}: ${subObject[subKey]}`);
+          }
+          segments.push(segment);
+        }
+        return segments;
+      }
+      return [];
+    },
+  },
   methods: {
     async submit() {
       const body = {
-        text: this.textInput
-      }
+        text: this.textInput,
+      };
 
-      const jsonBody = JSON.stringify(body)
+      const jsonBody = JSON.stringify(body);
 
       const response = await fetch('http://localhost:3000', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: jsonBody
-      })
+        body: jsonBody,
+      });
 
-      this.response = await response.json()
-    }
-  }
-}
+      this.response = await response.json();
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -75,6 +98,29 @@ export default {
 
 .response {
   margin-top: 20px;
-  color: #fff; /* White text color for the response */
+  color: #000000; /* White text color for the response */
+}
+
+.response-box {
+  background-color: #fff;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+}
+
+.response-title {
+  font-weight: bold;
+  text-decoration: underline;
+  font-size: 16px;
+}
+
+.response-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.response-list li {
+  font-size: 14px;
 }
 </style>
